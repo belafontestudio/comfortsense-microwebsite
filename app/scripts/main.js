e2dac9b4da1a5e52c4419eff3a1484602cafd020
nOttenga <ottenga.nic@gmail.com>
2015-06-23 10:59:40 +0200

    init mobile version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,39 @@
 
 $(document).ready(function() {
 
-  $("#owl-example").owlCarousel();
+  initLiving();
+  initApp();
 
 });
+
+
+
+function initGalleryHero(){
+  $(".gallery-hero").addClass('.owl-carousel');
+  $(".gallery-hero").owlCarousel({
+
+    singleItem : true,
+    itemsScaleUp : false
+
+  });
+}
+
+function initLiving(){
+
+  $(".living-lab-carousel").owlCarousel({
+
+    singleItem : true,
+    itemsScaleUp : false
+
+  });
+}
+
+function initApp(){
+
+  $(".app-carousel").owlCarousel({
+
+    singleItem : true,
+    itemsScaleUp : false
+
+  });
+}
